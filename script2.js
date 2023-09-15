c760bd44cb9a2d58a22097841befea3ccd97addb
Akshara-ch <119268681+Akshara-ch@users.noreply.github.com>
2023-09-15 19:03:56 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,9 @@
+// script.js
+
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    // You can add code here to handle form submission, e.g., sending data to a server or displaying a thank you message.
+    alert('Form submitted!');
+});
+
